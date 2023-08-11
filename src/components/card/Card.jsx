ce6640147059ba21756b45c8225c3c763e764b49
hetur <49845870+hetur@users.noreply.github.com>
2023-08-11 00:13:57 -0300

    cambios 110823

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,12 +1,20 @@
+import styles from "./Car.module.css"
+
 export default function Card(name, species, image, gender, onClose) {
-   
    return (
-      <div>
-         <button onClick={onClose}>X</button>
-         <h2>{name}</h2>
+      <div className={styles.container}>
+         <div className={styles.buttonContainer}>
+         <button onClick={onClose}>
+         X
+         </button>
+         </div>
+         <div className={styles.imageContainer}>
+         <img src={image} alt="not found" />
+         <h2 className={styles.name}>{name}</h2>
+         </div>
+
          <h2>{species}</h2>
          <h2>{gender}</h2>
-         <img src={image} alt="not found" />
 
          </div>
    );
